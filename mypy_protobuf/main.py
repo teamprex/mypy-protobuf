09711a5bca99d2cf68772c60b8e12f5c19dceb39
Taewoo An <taewoo@prestolabs.io>
2024-07-22 15:21:04 +0900

    feat: use AsyncGenerator

--- mypy_protobuf/main.py
+++ mypy_protobuf/main.py
@@ -691,7 +691,7 @@ class PkgWriter(object):
     def _output_type(self, method: d.MethodDescriptorProto, use_stream_iterator: bool = True) -> str:
         result = self._import_message(method.output_type)
         if use_stream_iterator and method.server_streaming:
-            result = f"{self._import('collections.abc', 'Iterator')}[{result}]"
+            result = f"{self._import('typing', 'AsyncGenerator')}[{result}, Any]"
         return result
 
     def write_grpc_methods(self, service: d.ServiceDescriptorProto, scl_prefix: SourceCodeLocation) -> None:
